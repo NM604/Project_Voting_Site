9ff3b5b997adfedcc153f571c17fbb020015277a
Nikhil Abraham Mathew <nikhilmath99@gmail.com>
2021-07-19 01:17:36 +0530

    Adds file to initialise db

---
+++ db.py
@@ -0,0 +1,38 @@
+import datetime
+import sqlite3
+from flask import current_app, g
+from flask.cli import with_appcontext
+import click
+import psycopg2
+
+def get_db():
+  if 'db' not in g: 
+    dbname = current_app.config['DATABASE']
+    g.db = sqlite3.connect(dbname)
+    g.db.execute("PRAGMA foreign_keys = ON;")
+  return g.db
+
+def close_db(e=None):
+  db = g.pop('db', None)
+  if db is not None:
+    db.close()
+    
+def init_db():
+  db = get_db()
+  f = current_app.open_resource("sql/initial.sql")
+  sql_code = f.read().decode("ascii")
+  cur = db.cursor()
+  cur.executescript(sql_code)
+  cur.close()
+  db.commit()
+  close_db()
+   
+@click.command('initdb', help="Initialise The Database")
+@with_appcontext
+def init_db_command():
+  init_db()
+  click.echo('Database Initialised')
+
+def init_app(app):
+  app.teardown_appcontext(close_db)
+  app.cli.add_command(init_db_command)
